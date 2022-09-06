f1d011c70d9021ae639672083c54adee4134c7c1
Heril Saha <herilsaha06@gmail.com>
2022-09-06 02:53:02 -0600

    Finished student_grade_assignment

--- student_grade_assignment/src/main.rs
+++ student_grade_assignment/src/main.rs
@@ -1,14 +1,98 @@
-// input output module from standard library crate
+// input output module used for taking input from user
 use std::io;
+// process module used for exiting program
+use std::process;
+// library to generate random numbers
+use rand::Rng;
 
-fn display_menu() {
+struct GradeBook {
+    grades: Vec<u16>
+}
+
+trait GradeOperations {
+    fn gen_grades() -> Vec<u16>; // static function used in randomize() and new()
+    fn new() -> GradeBook;
+    fn show_all(&self);
+    fn honours(&self);
+    fn max(&self) -> &u16;
+    fn min(&self) -> &u16;
+    fn avg(&self) -> u8;
+    fn stats(&self);
+    fn randomize(&mut self);
+}
+
+impl GradeOperations for GradeBook {
+    fn gen_grades() -> Vec<u16> {
+        let mut rng = rand::thread_rng();
+
+        let mut rand_grades: Vec<u16> = Vec::with_capacity(35);
+
+        for _ in 0..35 {
+            rand_grades.push(rng.gen_range(0..101)); // gen_range fn returns [0, 101) -> will not include 101
+        }
+
+        rand_grades
+    }
+
+    fn new() -> GradeBook {
+        GradeBook { grades: Self::gen_grades() }
+    }
+
+    fn show_all(&self) {
+        println!("ALL GRADES");
+        for i in &self.grades {
+            println!("{}%", i)
+        }
+    }
+
+    fn honours(&self) {
+        println!("HONOURS");
+        let mut count = 0;
+        for i in &self.grades {
+            if i >= &80 {
+                println!("{}%", i);
+                count += 1;
+            }
+        }
+        println!("Number of Honours: {}", count);
+    }
+
+    fn max(&self) -> &u16 {
+        self.grades.iter().max().unwrap()
+    }
+
+    fn min(&self) -> &u16 {
+        self.grades.iter().min().unwrap()
+    }
+
+    fn avg(&self) -> u8 {
+        let sum: u16 = self.grades.iter().sum();
+        let mean: f32 = sum as f32 / self.grades.len() as f32;
+        mean.round() as u8
+    }
+
+    fn stats(&self) {
+        println!("STATS");
+        println!("Highest Grade: {}%", self.max());
+        println!("Lowest Grade: {}%", self.min());
+        println!("Average Grade: {}%", self.avg());
+    }
+
+    fn randomize(&mut self) {
+        let new_grades = Self::gen_grades();
+        self.grades = new_grades;
+        println!("GRADES HAVE BEEN RANDOMIZED");
+    }
+}
+
+fn display_menu(grade_book: &mut GradeBook) {
     println!(r#"
-    MAIN MENU
-        1. Display All Grades
-        2. Display Honours
-        3. Stats
-        4. Randomize Grades
-        5. Exit
+MAIN MENU
+    1. Display All Grades
+    2. Display Honours
+    3. Stats
+    4. Randomize Grades
+    5. Exit
     "#);
 
     let mut input = String::new();
@@ -16,20 +100,23 @@ fn display_menu() {
     // make "input" variable = user input from terminal
     io::stdin().read_line(&mut input).expect("Failed to read line");
 
+    println!("\n");
     // remove trailing whitespace from input
     // (input from keyboard will always contain trailing newline character)
     match input.trim_end() {
-        "1" => println!("YOU CHOSE 1"),
-        "2" => println!("YOU CHOSE 2"),
-        "3" => println!("YOU CHOSE 3"),
-        "4" => println!("YOU CHOSE 4"),
-        "5" => println!("YOU CHOSE 5"),
+        "1" => grade_book.show_all(),
+        "2" => grade_book.honours(),
+        "3" => grade_book.stats(),
+        "4" => grade_book.randomize(),
+        "5" => process::exit(1),
         _ => println!("Invalid choice"),
     }
 }
 
 fn main() {
+    let mut grade_book = GradeBook::new();
+
     loop {
-        display_menu();
+        display_menu(&mut grade_book);
     }
 }
--- student_grade_assignment/target/debug/student_grade_assignment.d
+++ student_grade_assignment/target/debug/student_grade_assignment.d
@@ -1,1 +1,1 @@
-/home/herilsaha/ComSci30/student_grade_assignment/target/debug/student_grade_assignment: /home/herilsaha/ComSci30/student_grade_assignment/src/main.rs
+/Users/herilsaha/ComSci30/student_grade_assignment/target/debug/student_grade_assignment: /Users/herilsaha/ComSci30/student_grade_assignment/src/main.rs
