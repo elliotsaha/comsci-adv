1fd646af6dc8cbbc03c39c8fa00761f6eb07bfad
Heril Saha <herilsaha06@gmail.com>
2022-10-17 09:36:51 -0600

    selection sort

--- selection_sort_assignment/src/main.rs
+++ selection_sort_assignment/src/main.rs
@@ -1,3 +1,20 @@
+fn selection_sort<T: Ord>(arr: &mut [T]) {
+
+   for i in 0..arr.len() - 1 {
+       // search for minimum
+       let mut min = i;
+       for j in (i + 1)..arr.len() {
+           if arr[j] < arr[min] {
+               // set current to minimum
+               min = j;
+           }
+       }
+       arr.swap(min, i)
+   }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut numbers = [4, 65, 2, -31, 0, 99, 2, 83, 782, 1];
+    selection_sort(&mut numbers);
+    println!("{:?}", numbers);
 }
